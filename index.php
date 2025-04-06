4ab3c3cadbb6855cedc88eda0edec54870d76c3b
Adrian Alberto Luty <adrianalbertoluty@gmail.com>
2025-04-06 17:00:24 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Online Store</title>
+  <link rel="stylesheet" href="cart/UImin.css">
+</head>
+<body>
+<h2>Available Products</h2>
+<div id="countdown"></div>
+<div id="item"></div>
+<div>
+  <button onclick="pay()">Pay</button>
+  <button onclick="cancel()">Cancel</button>
+</div>
+<script src="cart/sys.js" charset="utf-8"></script>
+</body>
+</html>
