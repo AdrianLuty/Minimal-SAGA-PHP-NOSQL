ae5ade9f759d994f6d6911dd8395a7210f5cf4ce
Adrian Alberto Luty <adrianalbertoluty@gmail.com>
2025-04-06 16:53:12 -0300

    Delete stock.php

--- stock.php
+++
@@ -1,6 +0,0 @@
-<?php
-$product[478124879324]['available'] = 9;
-$product[123456789012]['available'] = "unlimited";
-$product[987654321098]['available'] = 2;
-$product[112233445566]['available'] = 4;
-$product[112233445366]['available'] = 4;
