5107d396ae55518208c9b2483b7d316f36184d2c
Adrian Alberto Luty <adrianalbertoluty@gmail.com>
2025-04-06 16:47:24 -0300

    Create sales.php

---
+++ sales/sales.php
@@ -0,0 +1,6 @@
+<?php
+$sold['44']['payment_44']['items']['123456789012']['quantity'] = 6;
+$sold['44']['payment_44']['items']['123456789012']['amount'] = $unitPrice;
+$sold['44']['payment_44']['items']['data']['transaction'] = 'transaction number';
+$sold['44']['payment_44']['items']['data']['date'] = '06/04/2025 03:04:58';
+$sold['44']['payment_44']['items']['data']['shipment'] = 'shipping tracking number';
